020ed4b970c2195238fd773f347e0495018ce1c5
Frampt <18995691365@189.cn>
2026-08-16 03:18:47 +0800

    [+]manifest.json

--- start.bat
+++ start.bat
@@ -5,7 +5,7 @@ echo   OPAD - Agile Dashboard
 echo ========================================
 echo.
 echo Starting server...
-echo Open: http://localhost:3001
+echo Open: http://127.0.0.1:3001
 echo Press Ctrl+C to stop
 echo.
 node server.js
